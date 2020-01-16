b901cc7b2dc32596ca601d4f8ebbfee24334400d
Ash Vince <amvince@gmail.com>
2020-01-16 09:05:46 -0800

    Playing with spin counting

---
+++ ColourWheel/src/main/java/frc/robot/commands/spinCounter.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ColourWheel;
+
+public class spinCounter extends CommandBase {
+  /**
+   * Creates a new spinCounter.
+   */
+  private ColourWheel m_wheel = new ColourWheel();
+  // private Color initColour, prevColour;
+  private String s_initColour, s_prevColour;
+
+  
+  private double counter;
+
+  public spinCounter() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    /* 
+    initColour = m_wheel.detectedColor();
+    prevColour = initColour;
+    */
+    counter = 0;
+    s_initColour = m_wheel.colourMatch();
+    s_prevColour = s_initColour;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_wheel.clockwise();
+    // This uses the "numbers" so it won't be perfect. Needs to be set for "matched" name instead.
+    //counter += ((m_wheel.detectedColor()==initColour) && (m_wheel.detectedColor() != prevColour)) ? 0.5 : 0;
+
+    /* This might be the code for a "named" colour counter. */
+    String currColour = m_wheel.colourMatch();
+    counter += ((currColour == s_initColour) && (currColour != s_prevColour)) ? 0.5 : 0;
+    SmartDashboard.putString("Detected Color", currColour);
+    SmartDashboard.putString("Spins", String.valueOf(counter/2));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_wheel.stop();
+    
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (counter == 3);
+  }
+}
--- ColourWheel/src/main/java/frc/robot/subsystems/ColourWheel.java
+++ ColourWheel/src/main/java/frc/robot/subsystems/ColourWheel.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.util.Color;
@@ -25,6 +27,12 @@ public class ColourWheel extends SubsystemBase {
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
   private Color detectedColor;
+  
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   public ColourWheel() {
 
@@ -57,6 +65,9 @@ public class ColourWheel extends SubsystemBase {
   public void stop() {
     spinner.set(0);
   }
+
+
+
   public double confidence() {
     // ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
     // detectedColor.confidence;
@@ -68,4 +79,22 @@ public class ColourWheel extends SubsystemBase {
     this.detectedColor = m_colorSensor.getColor();
     
   }
+
+public String colourMatch() {
+  String colorString;
+  Color detectedColor = m_colorSensor.getColor();
+  ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+  if (match.color == kBlueTarget) {
+    colorString = "Blue";
+  } else if (match.color == kRedTarget) {
+    colorString = "Red";
+  } else if (match.color == kGreenTarget) {
+    colorString = "Green";
+  } else if (match.color == kYellowTarget) {
+    colorString = "Yellow";
+  } else {
+    colorString = "Unknown";
+  }
+  return colorString;
+}
 }
